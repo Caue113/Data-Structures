ebf1e99c20d7332f9a6bc69d612b66e6d12ecbe9
Luis Felipe <luisftsat@gmail.com>
2024-02-21 21:12:27 -0300

    feat: array exercise level easy: to sort it

---
+++ Array/Exercises/sortArray.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *array_bubblesort_ascend(int *array, int length){
+    if(length < 1){
+        printf("[bubblesort] Array Size not Valid");
+        return array;
+    }
+    
+    for (size_t i = 0; i < length; i++)
+    {
+        for (size_t j = 1; j < length; j++)
+        {
+            int temp = 0;
+            if(array[j] < array[j-1]){
+                temp = array[j];
+                array[j] = array[j-1];
+                array[j-1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+int *array_bubblesort_descend(int *array, int length){
+    if(length < 1){
+        printf("[bubblesort] Array Size not Valid");
+        return array;
+    }
+    
+    for (size_t i = 0; i < length; i++)
+    {
+        for (size_t j = 1; j < length; j++)
+        {
+            int temp = 0;
+            if(array[j] > array[j-1]){
+                temp = array[j];
+                array[j] = array[j-1];
+                array[j-1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+void array_transverse(int *array, int length){
+    printf("The array is\n\t{");
+    for (size_t i = 0; i < length; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("}\n");
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    int array_size = 5;
+    int *arr1 = malloc(sizeof(int) * array_size);
+    arr1[0] = 18;
+    arr1[1] = 55535;
+    arr1[2] = 0;
+    arr1[3] = 2;
+    arr1[4] = 75;
+
+    //Show unsorted. Expects: 18, 55535, 0, 2, 75
+    array_transverse(arr1, array_size);
+
+    printf("Showing sorted array\n");
+
+    //Expects: 0 2 18 75 55535
+    array_bubblesort_ascend(arr1, array_size);
+    array_transverse(arr1, array_size);
+    return 0;
+}
